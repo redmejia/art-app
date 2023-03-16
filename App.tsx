f0ed386f6e6cc0bfde592386bbc70028c8ad6fb2
redmejia <reynaldoemejia@gmail.com>
2023-03-16 15:01:43 -0700

    artist section

--- App.tsx
+++ App.tsx
@@ -7,77 +7,85 @@
 
 import React from 'react';
 import {
-  FlatList,
-  Platform,
-  StyleSheet,
-  View,
+	FlatList,
+	Platform,
+	StyleSheet,
+	Text,
+	View,
 } from 'react-native';
 import SmCard from './src/Components/Utils/Card/SmCard';
 
 type Mockdata = {
-  src: any | undefined,
-  title: string,
+	src: any | undefined,
+	title: string,
 }
 
 
 const data: Mockdata[] = [
-  { src: require('../ArtApp/src/Components/public/steve-johnson-1.png'), title: 'Pastel' },
-  { src: require('../ArtApp/src/Components/public/steve-johnson-2.png'), title: 'Acrylic' },
-  { src: require('../ArtApp/src/Components/public/dan-farrell-3.png'), title: 'Pastel' },
-  { src: require('../ArtApp/src/Components/public/dan-farrell-4.png'), title: 'Abstract' }
+	{ src: require('../ArtApp/src/Components/public/steve-johnson-1.png'), title: 'Pastel' },
+	{ src: require('../ArtApp/src/Components/public/steve-johnson-2.png'), title: 'Acrylic' },
+	{ src: require('../ArtApp/src/Components/public/dan-farrell-3.png'), title: 'Pastel' },
+	{ src: require('../ArtApp/src/Components/public/dan-farrell-4.png'), title: 'Abstract' }
 ]
 
 
 function App(): JSX.Element {
-  return (
-    <View
-      style={[
-        styles.container,
-        styles.containerDirection,
-      ]}>
-      <View style={styles.topOptionArt} >
-        <FlatList
-          horizontal={true}
-          data={data}
-          renderItem={({ item }) =>
-            <View style={{ marginLeft: 20 }}>
-              <SmCard
-                Title={item.title}
-                Src={item.src}
-                TitleStyle={styles.textTitle}
-              />
-            </View>
-          }
-        />
-      </View>
-      <View style={{ flex: 2, backgroundColor: '#fff' }} />
-      <View style={{ flex: 3, backgroundColor: '#fff' }} />
-    </View>
-  );
+	return (
+		<View
+			style={[
+				styles.container,
+				styles.containerDirection,
+			]}>
+			<View style={styles.topOptionArt} >
+				<FlatList
+					horizontal={true}
+					data={data}
+					renderItem={({ item }) =>
+						<View style={{ marginLeft: 20 }}>
+							<SmCard
+								Title={item.title}
+								Src={item.src}
+								TitleStyle={styles.textTitle}
+							/>
+						</View>
+					}
+				/>
+			</View>
+			<View style={{ flex: 2, backgroundColor: 'orange' }} >
+					<Text style={styles.textArtist}>Artists</Text>
+			</View>
+			<View style={{ flex: 3, backgroundColor: '#fff' }} />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // padding: 20,
-  },
-  containerDirection : {
-      flexDirection: 'column',
-  },
-  topOptionArt: {
-    flex: 3,
-    borderBottomLeftRadius: 30,
-    borderBottomRightRadius: 30,
-    backgroundColor: '#A74592',
-    marginTop: Platform.OS === 'ios' ? 50 : 0,
-    padding: Platform.OS === 'ios' ? 10 : 10,
-  },
-  textTitle: {
-    fontWeight: '100',
-    fontSize: 28,
-    color: '#fff'
-  },
-
+	container: {
+		flex: 1,
+		// padding: 20,
+	},
+	containerDirection: {
+		flexDirection: 'column',
+	},
+	topOptionArt: {
+		flex: 3,
+		borderBottomLeftRadius: 30,
+		borderBottomRightRadius: 30,
+		backgroundColor: '#A74592',
+		marginTop: Platform.OS === 'ios' ? 50 : 0,
+		padding: Platform.OS === 'ios' ? 10 : 10,
+	},
+	textTitle: {
+		fontWeight: '100',
+		fontSize: 28,
+		color: '#fff'
+	},
+	textArtist : {
+		padding: 20,
+		fontWeight: '200',
+		fontSize: 20,
+		color: '#000'
+	}
 });
 
 export default App;
