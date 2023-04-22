5db12aaa4795192e3a74bf62b12d39241664b29e
redmejia <reynaldoemejia@gmail.com>
2023-04-22 16:11:40 -0700

    small change fetching data localhost

--- src/Graphql/client.config.tsx
+++ src/Graphql/client.config.tsx
@@ -1,5 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { BASE_URL } from "./uri";
+
 // use IP insted of localhost
 export const client = new ApolloClient({
     uri : BASE_URL,
